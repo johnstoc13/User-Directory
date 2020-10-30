ac7b268eeceb5ec3161bce2cc528778bc8abd2fb
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-10-30 12:51:08 -0400

    Directory component showing header!

---
+++ src/components/Directory/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Header from "../Header";
+
+class Directory extends Component {
+
+    render() {
+        return (
+            <div className="Directory-container">
+                <Header />
+            </div>
+        )
+    }
+}
+
+export default Directory;
