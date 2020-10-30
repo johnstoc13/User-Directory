f8fca4c06c5ca8ea5e10dfb5608dd9f852112600
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-10-30 13:36:35 -0400

    added styling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Grid from '@material-ui/core/Grid';
 import Typography from "@material-ui/core/Typography"
+import "./style.css";
 
 function Header() {
     return (
         <>
             <Grid className="Header-heading" container justify="center">
-                <Typography variant="h1" align="center">Employee Directory</Typography>
+                <Typography variant="h1" align="center" color="secondary">Employee Directory</Typography>
             </Grid>
         </>
     )
